5d3e54c1e6b3b60de287c10ca6c1b9c27db65e3e
Josh Mervine <joshua@mervine.net>
2017-10-07 10:16:26 -0700

    Using testify and cleaning up tests

--- splunking_test.go
+++ splunking_test.go
@@ -1,98 +1,72 @@
 package splunking
 
 import (
-	//"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/jarcoal/httpmock.v1"
 )
 
 func TestRequest(t *testing.T) {
+	assert := assert.New(t)
+
 	sr := defaultRequest()
 	r, e := sr.Request("GET", "/api/path", nil)
 
-	if e != nil {
-		t.Error("Expected nil, got,", e)
-	}
-
-	expected := r.Header.Get("Accept")
-	if expected != "application/json" {
-		t.Error("Expected application/json got,", expected)
-	}
-
-	expected = r.Header.Get("Authorization")
-	if !strings.HasPrefix(expected, "Basic ") {
-		t.Error("Expected Basic Authication got,", expected)
-	}
-
-	expected = "https://splunk.example.com:8089/api/path?output_mode=json"
-	if r.URL.String() != expected {
-		t.Error("Expected", expected, " got,", expected)
-	}
+	assert.Nil(e)
+	assert.Equal(r.Header.Get("Accept"), "application/json")
+	assert.True(strings.HasPrefix(r.Header.Get("Authorization"), "Basic "))
+	assert.Equal(r.URL.String(), "https://splunk.example.com:8089/api/path?output_mode=json")
 }
 
 func TestSubmit(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder("GET", "https://splunk.example.com:8089/api/path?output_mode=json",
-		httpmock.NewStringResponder(200, `{}`))
+	defer mockRequest("GET", "https://splunk.example.com:8089/api/path?output_mode=json")()
 
 	sr := defaultRequest()
 	req, _ := sr.Request("GET", "/api/path", nil)
+	resp, err := sr.Submit(req)
 
-	_, e := sr.Submit(req)
-	if e != nil {
-		t.Error("Expected nil, got,", e)
-	}
+	assertResponse(t, resp, err, "at=GET")
 }
 
 // TODO: Also testing params handling, should be broken out in to it's own
 // test.
 func TestGet(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder("GET", "https://splunk.example.com:8089/api/path?foo=bar&output_mode=somethingelse",
-		httpmock.NewStringResponder(200, `{}`))
+	defer mockRequest("GET", "https://splunk.example.com:8089/api/path?foo=bar&output_mode=somethingelse")()
 
 	sr := defaultRequest()
+	resp, err := sr.Get("/api/path?foo=bar&output_mode=somethingelse", nil)
 
-	_, e := sr.Get("/api/path?foo=bar&output_mode=somethingelse", nil)
-	if e != nil {
-		t.Error("Expected nil, got,", e)
-	}
+	assertResponse(t, resp, err, "at=GET")
 }
 
 func TestPost(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder("POST", "https://splunk.example.com:8089/api/path?output_mode=json",
-		httpmock.NewStringResponder(200, `{}`))
+	defer mockRequest("POST", "https://splunk.example.com:8089/api/path?output_mode=json")()
 
 	sr := defaultRequest()
+	resp, err := sr.Post("/api/path", nil)
 
-	_, e := sr.Post("/api/path", nil)
-	if e != nil {
-		t.Error("Expected nil, got,", e)
-	}
+	assertResponse(t, resp, err, "at=POST")
 }
 
 func TestDelete(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder("DELETE", "https://splunk.example.com:8089/api/path?output_mode=json",
-		httpmock.NewStringResponder(200, `{}`))
+	defer mockRequest("DELETE", "https://splunk.example.com:8089/api/path?output_mode=json")()
 
 	sr := defaultRequest()
 
-	_, e := sr.Delete("/api/path", nil)
-	if e != nil {
-		t.Error("Expected nil, got,", e)
-	}
+	resp, err := sr.Delete("/api/path", nil)
+
+	assertResponse(t, resp, err, "at=DELETE")
+}
+
+func mockRequest(method, url string) func() {
+	httpmock.Activate()
+	httpmock.RegisterResponder(method, url, httpmock.NewStringResponder(200, "at="+method))
+
+	return httpmock.DeactivateAndReset
 }
 
 func defaultRequest() *SplunkRequest {
@@ -102,5 +76,13 @@ func defaultRequest() *SplunkRequest {
 		Host:       "splunk.example.com",
 		Port:       "8089",
 		OutputMode: "json",
+	}
+}
+
+func assertResponse(t *testing.T, r *http.Response, e error, expected string) {
+	if assert.Nil(t, e) {
+		defer r.Body.Close()
+		body, _ := ioutil.ReadAll(r.Body)
+		assert.Equal(t, string(body), expected)
 	}
 }
